4b2cb3dc45aafffb26b8f0d746babf424c2c20a4
unknown <usamawizard@gmail.com>
2022-11-14 01:16:40 +0530

    sending and receiving streams working

---
+++ libs/build.sh
@@ -0,0 +1,7 @@
+windows 
+gcc -I ./ sendRecvAnt.c -o D:\\gstreamer\\1.0\\mingw_x86_64\\bin\\sendRecvAnt.exe ./libs/win64_librws.lib $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-webrtc-1.0 gstreamer-sdp-1.0  json-glib-1.0 )
+
+Mac
+gcc sendRecvAnt.c  -o  sendRecvAnt  `pkg-config --cflags --libs gstreamer-1.0 gstreamer-rtp-1.0 gstreamer-webrtc-1.0 gstreamer-sdp-1.0  json-glib-1.0  gstreamer-net-1.0  gstreamer-pbutils-1.0` ./mac_librws_static.a
+
+GST_DEBUG=3 D:\\gstreamer\\1.0\\mingw_x86_64\\bin\\sendRecvAnt.exe 10 

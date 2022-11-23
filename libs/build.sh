02b5e9143a18912160245aba60a61c4b59b32aef
unknown <usamawizard@gmail.com>
2022-11-23 19:36:09 +0530

    files removed

--- libs/build.sh
+++ libs/build.sh
@@ -3,7 +3,7 @@ gcc -I ./ sendRecvAnt.c -o D:\\gstreamer\\1.0\\mingw_x86_64\\bin\\sendRecvAnt.ex
 D:\\gstreamer\\1.0\\mingw_x86_64\\bin\\sendRecvAnt.exe  --ip AMS_IP
 
 Mac
-gcc sendRecvAnt.c  -o  sendRecvAnt  `pkg-config --cflags --libs gstreamer-1.0 gstreamer-webrtc-1.0 gstreamer-sdp-1.0  json-glib-1.0 ` ./mac_librws_static.a
+gcc sendRecvAnt.c  -o  sendRecvAnt  `pkg-config --cflags --libs gstreamer-1.0 gstreamer-webrtc-1.0 gstreamer-sdp-1.0  json-glib-1.0 ` ./mac_m1_librws_static.a
 ./sendRecvAnt  --ip AMS_IP
 
 
